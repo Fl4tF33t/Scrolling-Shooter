99aed4b994d7208a3188bd7d29ef5de76675be6d
Khairulhelmy Hussain <ab5823@student.jamk.fi>
2025-05-21 17:17:18 +0300

    Added main menu and scene loading

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using MEC;
+using UnityEngine.SceneManagement;
+
+namespace ScrollShooter {
+    public static class Loader {
+        public enum Scene {
+            MainMenu,
+            Loading,
+            Level1
+        }
+
+        public static void Load(Scene scene) {
+            SceneManager.LoadScene(nameof(Scene.Loading));
+            Timing.RunCoroutine(LoadTargetScene(scene));
+        }
+
+        private static IEnumerator<float> LoadTargetScene(Scene scene) {
+            yield return Timing.WaitForOneFrame;
+            SceneManager.LoadScene(scene.ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ScrollShooter {
+    public class MainMenuUI : MonoBehaviour {
+        [SerializeField] private Loader.Scene startingScene;
+        [SerializeField] private Button playButton;
+        [SerializeField] private Button quitButton;
+
+        private void Awake() {
+            playButton.onClick.AddListener(() => Loader.Load(startingScene));
+            quitButton.onClick.AddListener(() => Application.Quit());
+            Time.timeScale = 1f;
+        }
+    }
+}
